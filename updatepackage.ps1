88fa2b97334f6edb5106afc7e629814f50fcb603
Sean Hall <r.sean.hall@gmail.com>
2018-12-08 17:21:36 -0600

    Create Powershell script to update Nuget packages for packages.config style projects as well as PackageReference style projects.

---
+++ updatepackage.ps1
@@ -0,0 +1,70 @@
+param([string]$TargetFolder, [string]$PackageName, [string]$NewVersion)
+
+function updatePackagesConfig {
+    param($targetFile, [string]$packageName, [string]$newVersion)
+
+    #Write-Host 'packages' $targetFile
+    $xml = New-Object XML
+    $xml.Load($targetFile)
+    $query = "/packages/package[@id='" + $packageName + "']";
+    $packageNode = $xml.SelectSingleNode($query);
+    if ($packageNode) {
+        $previousVersion = $packageNode.Attributes['version'].Value
+        #Write-Host $previousVersion
+        if ($previousVersion -ne $newVersion) {
+            $packageNode.Attributes['version'].Value = $newVersion
+            $xml.Save($targetFile)
+        }
+    }
+}
+
+function updateProject {
+    param($targetFile, [string]$packageName, [string]$newVersion)
+
+    #Write-Host 'project' $targetFile
+    $changed = $false
+    $fileContents = [System.IO.File]::ReadAllText($targetFile) # project files should be small right?
+    $oldStyleMatches = [System.Text.RegularExpressions.Regex]::Matches($fileContents,  '\\packages\\' + $packageName + '\.([^\\]*)\\')
+    $oldStyleMatches | ForEach-Object {
+        $previousVersion = $_.Groups[1]
+        #Write-Host $previousVersion
+        $fileContents = $fileContents.Replace($packageName + '.' + $previousVersion, $packageName + '.' + $newVersion)
+        $changed = $true
+    }
+    # hmmm, don't want to use XML parsing for the whole document because it might make annoying formatting changes throughout the file
+    # let's assume PackageReference elements don't span multiple lines
+    $packageReferenceMatches = [System.Text.RegularExpressions.Regex]::Matches($fileContents,  '.*PackageReference.*' + $packageName + '.*')
+    $packageReferenceMatches | ForEach-Object {
+        $xml = New-Object XML
+        $xml.LoadXml($_.Value)
+        $query = "//PackageReference[@Include='" + $packageName + "']";
+        $packageReferenceNode = $xml.SelectSingleNode($query);
+        if ($packageReferenceNode) {
+            $previousVersion = $packageReferenceNode.Attributes['Version'].Value
+            #Write-Host $previousVersion
+            if ($previousVersion -ne $newVersion) {
+                $originalXml = $packageReferenceNode.OuterXml
+                $packageReferenceNode.Attributes['Version'].Value = $newVersion
+                $fileContents = $fileContents.Replace($originalXml, $packageReferenceNode.OuterXml)
+                $changed = $true
+            }
+        }
+    }
+    if ($changed) {
+        [System.IO.File]::WriteAllText($targetFile, $fileContents)
+    }
+}
+
+function updateNugetPackageVersion {
+    param([string]$targetFolder, [string]$packageName, [string]$newVersion)
+    
+    Get-ChildItem -Path $targetFolder -Include packages.config -Recurse | ForEach-Object {
+        updatePackagesConfig -targetFile $_ -packageName $packageName -newVersion $newVersion
+    }
+    
+    Get-ChildItem -Path $targetFolder -Include *.*proj -Recurse | ForEach-Object {
+        updateProject -targetFile $_ -packageName $packageName -newVersion $newVersion
+    }
+}
+
+updateNugetPackageVersion -targetFolder $TargetFolder -packageName $PackageName -newVersion $NewVersion
